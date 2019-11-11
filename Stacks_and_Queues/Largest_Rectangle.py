3812aa9c7a34f2b671d413a3d19859cfe4ea66a1
Nick <nikolayvaklinov@yahoo.com>
2019-11-11 19:53:33 +0000

    finding the area for building the mall

---
+++ Stacks_and_Queues/Largest_Rectangle.py
@@ -0,0 +1,19 @@
+
+
+def largestRectangle(h):
+    s = []
+    ans = len(h)
+    h.append(0)
+
+    for i in range(len(h)):
+        left_index = i
+        while len(s) > 0 and s[-1][0] >= h[i]:
+            last = s.pop()
+            left_index = last[1]
+            ans = max(ans, h[i] * (i + 1 - last[1]))
+            ans = max(ans, last[0] * (i - last[1]))
+        s.append((h[i], left_index))
+
+    return ans
+
+
