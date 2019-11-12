dac72791e20f19a5b79d5fb0ff54c2e295ddea76
Nick <nikolayvaklinov@yahoo.com>
2019-11-12 14:11:15 +0000

    using recursion for calculating big numbers' parts

---
+++ Recursion_and_Backtracking/Recursive_Digit_Sum.py
@@ -0,0 +1,14 @@
+def superDigit(n, k):
+    def add_digits(string):
+        if len(string) == 1:
+            return string
+        result = sum(int(s) for s in string)
+        return add_digits(str(result))
+
+    start = sum(int(s) for s in n) * k
+    return add_digits(str(start))
+
+
+# OR
+n, k = map(int, input().split())
+print( n * k % 9 or 9)
