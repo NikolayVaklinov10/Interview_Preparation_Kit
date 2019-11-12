51a67b75e792f0eab415dbe44441ce48540efbbe
Nick <nikolayvaklinov@yahoo.com>
2019-11-12 17:54:05 +0000

    merge point of two lists commit

---
+++ Linked_Lists/Find_Merge_Point_of_Two_Lists.py
@@ -0,0 +1,20 @@
+
+def findMergeNode(headA, headB):
+    curA = headA
+    curB = headB
+    while not curA == curB:
+        if curA.next is None:
+            curA = headB
+        else:
+            curA = curA.next
+        if curB.next is None:
+            curB = headA
+        else:
+            curB = curB.next
+    return curA.data
+
+
+
+
+
+
