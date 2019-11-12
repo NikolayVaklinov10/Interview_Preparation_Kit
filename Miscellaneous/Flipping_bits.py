03f79fe594903b3f424f9526c7d8017500883713
Nick <nikolayvaklinov@yahoo.com>
2019-11-12 18:01:48 +0000

    flipping commit

---
+++ Miscellaneous/Flipping_bits.py
@@ -0,0 +1,17 @@
+
+
+def flippingBits(n):
+
+    # this number is (2**32)-1
+    # unsigned integers given which is 32
+    # 2**32 = 4294967296
+    # to flip is to just minus 1
+    THE_FLIPPING = 4294967295
+
+    # ^ = bitwise XOR
+    return n ^ THE_FLIPPING
+
+
+
+
+
