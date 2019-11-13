aabdde80facd9160867124ba13d9a5e3d5ba2639
Nick <nikolayvaklinov@yahoo.com>
2019-11-13 13:23:58 +0000

    using defaultdict to solve the problem

--- Dictionaries_and_Hashmaps/Count_Triplets.py
+++ Dictionaries_and_Hashmaps/Count_Triplets.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def countTriplets(arr, r):
     counter={a: 0 for a in arr}
     tuplets={a: 0 for a in arr}
@@ -16,3 +19,13 @@ def countTriplets(arr, r):
 #  for counter and tuplets variables
 
 
+def countTriplets(arr, r):
+    v2 = defaultdict(int)
+    v3 = defaultdict(int)
+    count = 0
+    for k in arr:
+        count += v3[k]
+        v3[k*r] += v2[k]
+        v2[k*r] += 1
+
+    return count
